0ec5ca53875a7588f59a33c224179347d30247fa
nick <nik.npa@mail.ru>
2025-02-02 03:31:04 +0300

    v3 update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
 
-echo "Создаем виртуальное окружение..."
-python3 -m venv venv
+
+DIRECTORY="venv"
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Создаем виртуальное окружение..."
+    python3 -m venv venv
+fi
+
 
 echo "Активируем виртуальное окружение..."
 source venv/bin/activate
 
+
 if [ -f requirements.txt ]; then
-    echo "Устанавливаем зависимости из requirements.txt..."
-    pip install -r requirements.txt
+    PIP_FREEZE=$(pip freeze)
+    REQUIREMENTS=$(cat requirements.txt)
+    if [ "$PIP_FREEZE" != "$REQUIREMENTS" ]; then
+        echo "Устанавливаем зависимости из requirements.txt..."
+        pip install -r requirements.txt
+    fi
 fi
+
 
 cd labor_costs_tracking
 
+# проверка миграций
+UNAPPLIED_MIGRATIONS=$(python3 manage.py makemigrations | grep 'No changes detected' | wc -l)
+
+if [ "$UNAPPLIED_MIGRATIONS" -gt 0 ]; then
+    echo "Обнаружены неприменённые миграции. Применяем их..."
+    python manage.py migrate
+fi
+
+
 echo "Запускаем сервер Django..."
-python manage.py runserver
+python3 manage.py runserver
